819292f77d480c06d1d0cfd1156746c89c285011
mkrkru <mkrkru@mail.ru>
2021-08-09 11:48:38 +0300

    adding telegram auth

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,12 @@ import AnalyticsIcon from './assets/analytics_icon.png'
 function App() {
   return (
     <AdminPanelLayout barComponents={[
+      // {
+      //   render : <RegComponent/>,
+      //   icon : "",
+      //   title : "Registration",
+      //   path : "/"
+      // },
       {
         render : <FetchAnalyticsComponent/>,
         icon : AnalyticsIcon,
@@ -27,3 +33,18 @@ function App() {
 }
 
 export default App;
+
+/*
+<script async src="https://telegram.org/js/telegram-widget.js?15" data-telegram-login="goloads_auth_bot"
+    data-size="small" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+<script type="text/javascript">
+    function onTelegramAuth(user) {
+        alert(`First name: ${user.first_name}
+        Last name: ${user.last_name}
+        ID: ${user.id}
+        Username: @${user.username}
+        `);
+        // alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+    }
+</script>
+*/
