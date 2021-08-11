5f0b529ee693808ff3ba6593a6c2cacdbe61b1a2
mkrkru <mkrkru@mail.ru>
2021-08-11 16:35:51 +0300

    update fetch

--- src/adminpanel/Layout.tsx
+++ src/adminpanel/Layout.tsx
@@ -4,9 +4,9 @@ import './Layout.css';
 import { BrowserRouter, Route, Link, Redirect } from 'react-router-dom';
 import { Title } from "./TitleComponent";
 import TelegramLoginButton from 'react-telegram-login';
-import cookie from 'cookie_js'
+import cookie from 'cookie_js';
 import { getTelegramIcon, isAllDefined, isTelegramUserDefined, setTelegramUser, setUserCookie } from "../common/Storage";
-import { promiseRegister, toTelegramCallbackUser } from "../back/Register";
+// import { promiseRegister } from "../back/Register";
 
 export interface AdminPanelBarComponent {
     render: JSX.Element | React.Component | Element
@@ -26,72 +26,84 @@ export class AdminPanelLayout extends React.Component<AdminPanelLayoutProps> {
     }
 
     handleResponse(user: any) {
-        setTelegramUser(user)
-        promiseRegister(toTelegramCallbackUser(user))
-            .then(data => {
-                if (data !== null) {
-                    setUserCookie(data.cookie)
-                }
+        setTelegramUser(user);
+        fetch("https://doats.ml:8080/register", {
+            method: "POST",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                authDate: user.auth_date,
+                firstName: user.first_name,
+                lastName: user.last_name,
+                hash: user.hash,
+                id: user.id,
+                photoUrl: user.photo_url,
+                username: user.username
             })
-        this.forceUpdate()
+        }); // .then(data => data ? setUserCookie(data) : null);
+
+        this.forceUpdate();
     }
 
-    render() {
-        if (!isAllDefined()) {
-            return <div
-                className="Flex-center Center"
-                style={{
-                    color : "white",
-                    fontSize : "200px",
-                    width: "100%",
-                    height: "100vh",
-                    backgroundColor: "#1C1C1C"
-                }}
-            >
-                Login to access
-                <TelegramLoginButton dataOnauth={(user: any) => this.handleResponse(user)} botName="goloads_auth_bot" />
-            </div>
-        }
-        return <BrowserRouter>
-            <Redirect exact from="/" to="/analytics" />
-            <div className="AdminLayout">
-                <div className="AdminLayoutHeader">
-                    {
-                        this.props.barComponents.map((value, _, __) => <Route exact path={value.path}>
-                            <Title title={value.title} />
-                        </Route>
-                        )
-                    }
-                    <UserComponent
-                        icon={getTelegramIcon()}
-                        className="AdminLayoutUserHeader"
-                    />
-                </div>
-                <div className="AdminLayoutBody">
-                    <div className="AdminLayoutLeftBar">
-                        <div className="AdminLayoutLeftBarMargin" />
-                        {
-                            this.props.barComponents.map((component, _, __) => {
-                                return <Link to={component.path}>
-                                    <div className="AdminLayoutButton">
-                                        <img src={component.icon} className="Circle AdminLayoutButtomImage" />
-                                    </div>
-                                </Link>
-                            })
-                        }
-                    </div>
-                    <div className="AdminLayoutContentBody">
-                        {
-                            this.props.barComponents.map((component, _, __) => {
-                                return <Route exact path={component.path}>
-                                    {component.render}
-                                </Route>
-                            })
-                        }
-                    </div>
-                </div>
-            </div>
-        </BrowserRouter>
+render() {
+    if (!isAllDefined()) {
+        return <div
+            className="Flex-center Center"
+            style={{
+                color: "white",
+                fontSize: "200px",
+                width: "100%",
+                height: "100vh",
+                backgroundColor: "#1C1C1C"
+            }}
+        >
+            Login to access
+            <TelegramLoginButton dataOnauth={this.handleResponse} botName="goloads_auth_bot" />
+        </div>
     }
+    return <BrowserRouter>
+        <Redirect exact from="/" to="/analytics" />
+        <div className="AdminLayout">
+            <div className="AdminLayoutHeader">
+                {
+                    this.props.barComponents.map((value, _, __) => <Route exact path={value.path}>
+                        <Title title={value.title} />
+                    </Route>
+                    )
+                }
+                <UserComponent
+                    icon={getTelegramIcon()}
+                    className="AdminLayoutUserHeader"
+                />
+            </div>
+            <div className="AdminLayoutBody">
+                <div className="AdminLayoutLeftBar">
+                    <div className="AdminLayoutLeftBarMargin" />
+                    {
+                        this.props.barComponents.map((component, _, __) => {
+                            return <Link to={component.path}>
+                                <div className="AdminLayoutButton">
+                                    <img src={component.icon} className="Circle AdminLayoutButtomImage" />
+                                </div>
+                            </Link>
+                        })
+                    }
+                </div>
+                <div className="AdminLayoutContentBody">
+                    {
+                        this.props.barComponents.map((component, _, __) => {
+                            return <Route exact path={component.path}>
+                                {component.render}
+                            </Route>
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    </BrowserRouter>
+}
 
 }
--- src/back/Register.ts
+++ src/back/Register.ts
@@ -1,39 +1,9 @@
 export interface TelegramCallbackUser {
-    authDate: number
-    firstName: string
-    lastName: string
-    hash: string
-    id: number
-    photoUrl: string
+    authDate: number,
+    firstName: string,
+    lastName: string,
+    hash: string,
+    id: number,
+    photoUrl: string,
     username: string
-}
-
-export function promiseRegister(user: TelegramCallbackUser): Promise<any> {
-    return fetch("https://doats.ml:8080/register", {
-        method: "POST",
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Accept": "application/json",
-            "Content-type": "application/json"
-        },
-        body: JSON.stringify(user)
-    })
-        .then(response => {
-            if (response.ok) {
-                return response.json()
-            }
-            return null
-        })
-}
-
-export function toTelegramCallbackUser(user: any): TelegramCallbackUser {
-    return {
-        authDate: user.auth_date,
-        firstName: user.first_name,
-        lastName: user.last_name,
-        hash: user.hash,
-        id: user.id,
-        photoUrl: user.photo_url,
-        username: user.username
-    }
 }
